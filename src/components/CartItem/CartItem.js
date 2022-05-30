50ebf9d3bf49cce81d66c9e4a26ea7af9841b754
luongphamho <phamholuong@gmail.com>
2022-05-31 01:55:42 +0700

    add animation remove item from cart, complete& ready for submit

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { ADD_ITEM, REMOVE_FROM_CART, SUB_ITEM } from '../../app/types';
 import './CartItem.css'
 export default function CartItem(props) {
     const { item } = props;
     const dispatch = useDispatch();
+    const [isDelete, setIsDelete] = useState(false)
+    const [isShowRight, setIsShowRight] = useState(false)
+    useEffect(() => {
+        console.log(item)
+    }, [isDelete])
+    const hanleDelete = () => {
+        setIsDelete(true);
+        setTimeout(function () {
+            dispatch({
+                type: REMOVE_FROM_CART,
+                id: item.id
+            })
+            setIsDelete(false);
+        }, 1000);
+    }
+
+    setTimeout(function () {
+        setIsShowRight(true)
+    }, 500);
 
     return (
-        <div className="cart__item">
+        <div className={`cart__item ${isDelete ? 'delete-cart' : ''}`}>
             <div className="cart__item__left">
                 <div className="card__item__img" style={{ backgroundColor: `${item.color}`, width: '90px', height: '90px', borderRadius: '100%' }}>
                     <div className='cart__item__img__block'><img src={item.image} alt={item.name} /></div>
                 </div>
             </div>
-            <div className="cart__item__right">
+            <div style={{ display: isShowRight ? 'block' : 'none' }} className="cart__item__right">
                 <div className="cart__item__name">{item.name}</div>
                 <div className="cart__item__price">${item.price}</div>
                 <div className="cart__item__actions">
@@ -36,12 +55,7 @@ export default function CartItem(props) {
                             <img src="./assets/plus.png" alt="..." width={8} height={8} />
                         </button>
                     </div>
-                    <button className="cart__remove__button" onClick={() => {
-                        dispatch({
-                            type: REMOVE_FROM_CART,
-                            id: item.id
-                        })
-                    }}>
+                    <button className="cart__remove__button" onClick={hanleDelete}>
                         <img src="./assets/trash.png" alt="..." width={16} height={16} />
                     </button>
                 </div>
